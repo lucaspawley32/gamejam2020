e71445859b347a85e4f741e9281cf64f2775a064
Shalin Lathigra <shalinlathigra@gmail.com>
2020-01-31 22:22:09 -0500

    Add switch object, prep for pick up objects

---
+++ Assets/scripts/PickUpController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+	[SerializeField]
+	private bool isHeld;
+	[SerializeField]
+	private float heldDist;
+	[SerializeField]
+	GameObject holder;
+    // Start is called before the first frame update
+	
+    void Start()
+    {
+        isHeld = false;
+		holder = this.gameObject;
+    }
+
+	public bool IsHeld()
+	{
+		return isHeld;
+	}
+
+	public void SetHolder(GameObject other)
+	{
+		holder = other;
+		isHeld = true;
+	}
+
+	public void Drop()
+	{
+		holder = this.gameObject;
+		isHeld = false;
+	}
+}
